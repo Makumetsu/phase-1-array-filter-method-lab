e604fe19b7694d3a2bb75e0c70f89de163423065
Mohamed Osoble <ossoble360@gmail.com>
2023-10-17 12:24:55 -0400

    Working with arrays is a littl confusing

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(array1, names) {
+    return array1.filter(function (element) {
+      return element.toLowerCase() === names.toLowerCase();
+    });
+  }
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(function (driver) {
+      return driver.startsWith(letters);
+    });
+  }
+  function matchName(drivers, name) {
+    return drivers.filter(function (driver) {
+      return driver.name === name;
+    });
+  }
